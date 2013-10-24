f6e5be3fb31fa7032148b57249205aa5f38c3114
GJ <gjmcferrin@gmail.com>
2013-10-24 13:09:02 -0400

    Axe events.

--- src/main/java/com/gmail/nossr50/skills/axes/AxesManager.java
+++ src/main/java/com/gmail/nossr50/skills/axes/AxesManager.java
@@ -8,8 +8,11 @@ import org.bukkit.inventory.ItemStack;
 
 import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.datatypes.player.McMMOPlayer;
-import com.gmail.nossr50.datatypes.skills.AbilityType;
 import com.gmail.nossr50.datatypes.skills.SkillType;
+import com.gmail.nossr50.events.skills.axes.McMMOPlayerAxeMasteryEvent;
+import com.gmail.nossr50.events.skills.axes.McMMOPlayerCriticalHitEvent;
+import com.gmail.nossr50.events.skills.axes.McMMOPlayerGreaterImpactEvent;
+import com.gmail.nossr50.events.skills.axes.McMMOPlayerImpactEvent;
 import com.gmail.nossr50.locale.LocaleLoader;
 import com.gmail.nossr50.skills.SkillManager;
 import com.gmail.nossr50.util.ItemUtils;
@@ -25,24 +28,20 @@ public class AxesManager extends SkillManager {
         super(mcMMOPlayer, SkillType.AXES);
     }
 
-    public boolean canUseAxeMastery() {
-        return Permissions.bonusDamage(getPlayer(), skill);
+    private boolean canUseAxeMastery(LivingEntity target) {
+        return target.isValid() && Permissions.bonusDamage(getPlayer(), skill);
     }
 
-    public boolean canCriticalHit(LivingEntity target) {
-        return target.isValid() && Permissions.criticalStrikes(getPlayer());
+    private boolean canCriticalHit(LivingEntity target) {
+        return target.isValid() && Permissions.criticalStrikes(getPlayer()) && SkillUtils.activationSuccessful(getSkillLevel(), getActivationChance(), Axes.criticalHitMaxChance, Axes.criticalHitMaxBonusLevel);
     }
 
-    public boolean canImpact(LivingEntity target) {
-        return target.isValid() && Permissions.armorImpact(getPlayer()) && Axes.hasArmor(target);
+    private boolean canImpact(LivingEntity target) {
+        return target.isValid() && Axes.hasArmor(target) && Permissions.armorImpact(getPlayer());
     }
 
-    public boolean canGreaterImpact(LivingEntity target) {
-        return target.isValid() && Permissions.greaterImpact(getPlayer()) && !Axes.hasArmor(target);
-    }
-
-    public boolean canUseSkullSplitter(LivingEntity target) {
-        return target.isValid() && mcMMOPlayer.getAbilityMode(AbilityType.SKULL_SPLITTER) && Permissions.skullSplitter(getPlayer());
+    private boolean canUseGreaterImpact(LivingEntity target) {
+        return target.isValid() && !Axes.hasArmor(target) && Permissions.greaterImpact(getPlayer()) && (Axes.greaterImpactChance > Misc.getRandom().nextInt(getActivationChance()));
     }
 
     /**
@@ -51,9 +50,18 @@ public class AxesManager extends SkillManager {
      * @param target The {@link LivingEntity} being affected by the ability
      */
     public double axeMastery(LivingEntity target) {
-        double axeBonus = Math.min(getSkillLevel() / (Axes.bonusDamageMaxBonusLevel / Axes.bonusDamageMaxBonus), Axes.bonusDamageMaxBonus);
+        if (!canUseAxeMastery(target)) {
+            return 0;
+        }
 
-        return CombatUtils.callFakeDamageEvent(getPlayer(), target, axeBonus);
+        McMMOPlayerAxeMasteryEvent event = new McMMOPlayerAxeMasteryEvent(getPlayer(), target, calculateAxeMasteryBonus());
+        mcMMO.p.getServer().getPluginManager().callEvent(event);
+
+        if (event.isCancelled()) {
+            return 0;
+        }
+
+        return event.getDamage();
     }
 
     /**
@@ -63,24 +71,27 @@ public class AxesManager extends SkillManager {
      * @param damage The amount of damage initially dealt by the event
      */
     public double criticalHit(LivingEntity target, double damage) {
-        if (!SkillUtils.activationSuccessful(getSkillLevel(), getActivationChance(), Axes.criticalHitMaxChance, Axes.criticalHitMaxBonusLevel)) {
+        if (!canCriticalHit(target)) {
             return 0;
         }
 
         Player player = getPlayer();
-
         player.sendMessage(LocaleLoader.getString("Axes.Combat.CriticalHit"));
 
-        if (target instanceof Player) {
+        boolean targetIsPlayer = target instanceof Player;
+
+        if (targetIsPlayer) {
             ((Player) target).sendMessage(LocaleLoader.getString("Axes.Combat.CritStruck"));
-
-            damage = (damage * Axes.criticalHitPVPModifier) - damage;
-        }
-        else {
-            damage = (damage * Axes.criticalHitPVEModifier) - damage;
         }
 
-        return CombatUtils.callFakeDamageEvent(player, target, damage);
+        McMMOPlayerCriticalHitEvent event = new McMMOPlayerCriticalHitEvent(player, target, calculateCriticalHitBonus(damage, targetIsPlayer));
+        mcMMO.p.getServer().getPluginManager().callEvent(event);
+
+        if (event.isCancelled()) {
+            return 0;
+        }
+
+        return event.getDamage();
     }
 
     /**
@@ -88,18 +99,25 @@ public class AxesManager extends SkillManager {
      *
      * @param target The {@link LivingEntity} being affected by Impact
      */
-    public void impactCheck(LivingEntity target) {
+    public void impact(LivingEntity target) {
+        if (!canImpact(target)) {
+            return;
+        }
+
+        Player player = getPlayer();
         int durabilityDamage = 1 + (getSkillLevel() / Axes.impactIncreaseLevel);
+        McMMOPlayerImpactEvent event;
 
         for (ItemStack armor : target.getEquipment().getArmorContents()) {
             if (ItemUtils.isArmor(armor) && Axes.impactChance > Misc.getRandom().nextInt(getActivationChance())) {
-                Material armorType = armor.getType();
-                double durabilityModifier = 1 / (armor.getEnchantmentLevel(Enchantment.DURABILITY) + 1); // Modifier to simulate the durability enchantment behavior
-                double modifiedDurabilityDamage = durabilityDamage * durabilityModifier;
-                short maxDurability = mcMMO.getRepairableManager().isRepairable(armorType) ? mcMMO.getRepairableManager().getRepairable(armorType).getMaximumDurability() : armorType.getMaxDurability();
-                double maxDurabilityDamage = maxDurability * Axes.impactMaxDurabilityModifier;
+                event = new McMMOPlayerImpactEvent(player, armor, calculateImpactDurabilityDamage(durabilityDamage, armor));
+                mcMMO.p.getServer().getPluginManager().callEvent(event);
 
-                armor.setDurability((short) (Math.min(modifiedDurabilityDamage, maxDurabilityDamage) + armor.getDurability()));
+                if (event.isCancelled()) {
+                    continue;
+                }
+
+                armor.setDurability((short) (event.getDurabilityDamage() + armor.getDurability()));
             }
         }
     }
@@ -110,14 +128,21 @@ public class AxesManager extends SkillManager {
      * @param target The {@link LivingEntity} being affected by the ability
      */
     public double greaterImpact(LivingEntity target) {
-        if (!(Axes.greaterImpactChance > Misc.getRandom().nextInt(getActivationChance()))) {
+        if (!canUseGreaterImpact(target)) {
             return 0;
         }
 
         Player player = getPlayer();
 
+        McMMOPlayerGreaterImpactEvent event = new McMMOPlayerGreaterImpactEvent(player, target, Axes.greaterImpactBonusDamage, player.getLocation().getDirection().normalize().multiply(Axes.greaterImpactKnockbackMultiplier));
+        mcMMO.p.getServer().getPluginManager().callEvent(event);
+
+        if (event.isCancelled()) {
+            return 0;
+        }
+
         ParticleEffectUtils.playGreaterImpactEffect(target);
-        target.setVelocity(player.getLocation().getDirection().normalize().multiply(Axes.greaterImpactKnockbackMultiplier));
+        target.setVelocity(event.getKnockbackVelocity());
 
         if (mcMMOPlayer.useChatNotifications()) {
             player.sendMessage(LocaleLoader.getString("Axes.Combat.GI.Proc"));
@@ -131,7 +156,7 @@ public class AxesManager extends SkillManager {
             }
         }
 
-        return CombatUtils.callFakeDamageEvent(player, target, Axes.greaterImpactBonusDamage);
+        return event.getDamage();
     }
 
     /**
@@ -140,7 +165,26 @@ public class AxesManager extends SkillManager {
      * @param target The {@link LivingEntity} being affected by the ability
      * @param damage The amount of damage initially dealt by the event
      */
-    public void skullSplitterCheck(LivingEntity target, double damage) {
+    public void skullSplitter(LivingEntity target, double damage) {
         CombatUtils.applyAbilityAoE(getPlayer(), target, damage / Axes.skullSplitterModifier, skill);
+    }
+
+    private double calculateAxeMasteryBonus() {
+        return Math.min(getSkillLevel() / (Axes.bonusDamageMaxBonusLevel / Axes.bonusDamageMaxBonus), Axes.bonusDamageMaxBonus);
+    }
+
+    private double calculateCriticalHitBonus(double damage, boolean isPlayer) {
+        return (damage * (isPlayer ? Axes.criticalHitPVPModifier : Axes.criticalHitPVEModifier)) - damage;
+    }
+
+    private short calculateImpactDurabilityDamage(int durabilityDamage, ItemStack armor) {
+        Material armorType = armor.getType();
+
+        double durabilityModifier = 1 / (armor.getEnchantmentLevel(Enchantment.DURABILITY) + 1); // Modifier to simulate the durability enchantment behavior
+        double modifiedDurabilityDamage = durabilityDamage * durabilityModifier;
+        short maxDurability = mcMMO.getRepairableManager().isRepairable(armorType) ? mcMMO.getRepairableManager().getRepairable(armorType).getMaximumDurability() : armorType.getMaxDurability();
+        double maxDurabilityDamage = maxDurability * Axes.impactMaxDurabilityModifier;
+
+        return (short) (Math.min(modifiedDurabilityDamage, maxDurabilityDamage));
     }
 }
